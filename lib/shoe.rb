18e5bb7fbcc591769dd7e0df63dad608572c70b6
Candice Parker <lexus2424@gmail.com>
2019-05-27 23:25:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,4 +3,16 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  
+  def brand=(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(color)
+    
+  
 end
